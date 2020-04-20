5288df0beabf7f6e17b89b9a075055b88a3435c1
Anton Barkov <barkov.work@gmail.com>
2020-04-21 01:19:20 +0300

    FMPFeedbackSender.h & FMPZendeskFeedbackSender.h docs

--- FMPFeedbackForm/Sources/Model/FeedbackSender/FMPFeedbackSender.h
+++ FMPFeedbackForm/Sources/Model/FeedbackSender/FMPFeedbackSender.h
@@ -15,11 +15,22 @@ typedef void (^FMPSendFeedbackCompletion)(NSError *_Nullable error);
 
 @protocol FMPFeedbackSender <NSObject>
 
+/// The method that sends the data submitted by the form to a desired location.
+/// @param parameters A dictionary containing all the data submitted by the form. See @c FMPFeedbackParameter enum for the list of possible keys.
+/// @param completion A block that should be executed after data sending attempt, has nullable @c NSError as a parameter.
+/// You should pass an error if something fails, otherwise execute it with @c nil.
 - (void)sendFeedbackWithParameters:(NSDictionary<FMPFeedbackParameter, id> *)parameters
                         completion:(FMPSendFeedbackCompletion)completion;
 
 @optional
+/// The number of files that can be attached by the user.
+/// @discussion If the user selects more files than allowed by this number — they will be ignored by the form without any visual notification.
+/// @note A system profile text file is not accounted for in this number.
+///       For example, if user allowed attachments are limited to 10 files — a total of 11 files (including the system profile file) may be submitted by the form.
 @property (nonatomic, assign) NSUInteger maxAttachmentsCount;
+
+/// The maximum size in megabytes of a single file attached by the user.
+/// @discussion If the user selects files larger than the size allowed by this number — they will be ignored by the form without any visual notification.
 @property (nonatomic, assign) NSUInteger maxAttachmentFileSize;
 
 @end
