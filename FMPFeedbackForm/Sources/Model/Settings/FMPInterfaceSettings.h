04d48e941874b1122692a97d6e1d005f09800647
Anton Barkov <barkov.work@gmail.com>
2020-04-21 00:08:54 +0300

    FMPInterfaceSettings.h docs

--- FMPFeedbackForm/Sources/Model/Settings/FMPInterfaceSettings.h
+++ FMPFeedbackForm/Sources/Model/Settings/FMPInterfaceSettings.h
@@ -16,15 +16,40 @@ NS_ASSUME_NONNULL_BEGIN
 
 // Don't forget to update FMPInterfaceSettings+ObservableProperties when you add new properties
 
+/// An icon that is displayed in the top left corner of the form (most likely your app icon).
+/// @discussion If @c nil (default value) - no icon is displayed.
 @property (nonatomic, strong) NSImage *icon;
+
+/// Size of the icon that is displayed in the top left corner of the form.
+/// @discussion Defaults to 64x64.
 @property (nonatomic, assign) NSSize iconSize;
+
+/// A string that is displayed in the form's title label.
 @property (nonatomic, copy) NSString *title;
+
+/// A string that is displayed in the form's subtitle label.
 @property (nonatomic, copy) NSString *subtitle;
+
+/// An array of strings representing support ticket subjects.
 @property (nonatomic, copy) NSArray<NSString *> *subjectOptions;
+
+/// A string value of the form's name field placeholder.
 @property (nonatomic, copy) NSString *namePlaceholder;
+
+/// A string value of the form's email field placeholder.
 @property (nonatomic, copy) NSString *emailPlaceholder;
+
+/// A string value of the form's details field placeholder.
 @property (nonatomic, copy) NSString *detailsPlaceholder;
+
+/// A string value to set in the form's name field.
+/// @discussion Use this property in case you already know the user's name
+///             and want to prefill the form for him.
 @property (nonatomic, copy) NSString *defaultName;
+
+/// A string value to set in the form's email field.
+/// @discussion Use this property in case you already know the user's email
+///             and want to prefill the form for him.
 @property (nonatomic, copy) NSString *defaultEmail;
 
 @end
