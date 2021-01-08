70e786aab2673b7863ebce86365a0c9397b5ec5f
Serhii Butenko <serejahh@macpaw.com>
2021-01-08 18:34:37 +0200

    Allow up to 64 symbols in the top level domain

--- FMPFeedbackForm/Sources/Extensions/NSString+FMPExtensions.m
+++ FMPFeedbackForm/Sources/Extensions/NSString+FMPExtensions.m
@@ -12,7 +12,7 @@
 
 - (BOOL)fmp_isValidEmail
 {
-    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}";
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
     return [emailTest evaluateWithObject:self];
 }
