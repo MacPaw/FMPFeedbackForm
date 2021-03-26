3e01d22e64361d73fe9c8a1ed27195d99544c098
Serhii Butenko <sereja.butenko@gmail.com>
2021-03-26 15:23:43 +0200

    Add tests for email validation

---
+++ FMPFeedbackFormTests/NSString+FMPExtensionsTests.m
@@ -0,0 +1,46 @@
+//
+//  NSString+FMPExtensionsTests.m
+//  FMPFeedbackFormTests
+//
+//  Created by Serhii Butenko on 26.03.2021.
+//  Copyright © 2021 MacPaw. All rights reserved.
+//
+
+@import XCTest;
+#import "NSString+FMPExtensions.h"
+
+@interface NSString_FMPExtensionsTests : XCTestCase
+
+@end
+
+@implementation NSString_FMPExtensionsTests
+
+- (void)testValidEmails
+{
+    NSArray<NSString *> *emails = @[@"cocoa@macpaw.engineering",
+                                    @"cocoa@macpaw.com",
+                                    @"john..doe@gmail.com",
+                                    @"john.doe@macpaw.com.ua",
+                                    @"x@x.com"];
+    
+    for (NSString *email in emails)
+    {
+        XCTAssertTrue([email fmp_isValidEmail], @"%@ should be a valid email", email);
+    }
+}
+
+- (void)testInvalidEmails
+{
+    NSArray<NSString *> *emails = @[@"@outlook.com",
+                                    @"john doe@gmail.com",
+                                    @"just a word",
+                                    @"john.doe@.gmail.com",
+                                    @"стус@поети.укр"];
+    
+    for (NSString *email in emails)
+    {
+        XCTAssertFalse([email fmp_isValidEmail], @"%@ should be an invalid email", email);
+    }
+}
+
+@end
