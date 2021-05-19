57311be663a4d9ed5d4bc9b4f363718955ec6b29
Denys Telezhkin <strangervir@gmail.com>
2021-05-19 16:32:48 +0300

    Add special SPM header instead of symlinking to framework header to work around build issue with SPM.

---
+++ FMPFeedbackForm/include/FMPFeedbackForm.h
@@ -0,0 +1,14 @@
+//
+//  FMPFeedbackForm.h
+//  FMPFeedbackForm
+//
+//  Created by Anton Barkov on 21.01.2020.
+//  Copyright © 2020 MacPaw. All rights reserved.
+//
+
+// Imports For Swift Package Manager are required to be double-quoted.
+#import "FMPFeedbackParameter.h"
+#import "FMPFeedbackSender.h"
+#import "FMPZendeskFeedbackSender.h"
+#import "FMPFeedbackController.h"
+#import "FMPInterfaceSettings.h"
