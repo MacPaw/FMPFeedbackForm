abb80c29913322cd928f3bd982691e4130cb5b69
Denys Telezhkin <strangervir@gmail.com>
2021-05-19 16:32:00 +0300

    Remove if SWIFT_PACKAGE from framework header, remove symlink from SPM include folder.

--- FMPFeedbackForm/FMPFeedbackForm.h
+++ FMPFeedbackForm/FMPFeedbackForm.h
@@ -6,16 +6,8 @@
 //  Copyright © 2020 MacPaw. All rights reserved.
 //
 
-#if SWIFT_PACKAGE
-#import "FMPFeedbackParameter.h"
-#import "FMPFeedbackSender.h"
-#import "FMPZendeskFeedbackSender.h"
-#import "FMPFeedbackController.h"
-#import "FMPInterfaceSettings.h"
-#else
 #import <FMPFeedbackForm/FMPFeedbackParameter.h>
 #import <FMPFeedbackForm/FMPFeedbackSender.h>
 #import <FMPFeedbackForm/FMPZendeskFeedbackSender.h>
 #import <FMPFeedbackForm/FMPFeedbackController.h>
 #import <FMPFeedbackForm/FMPInterfaceSettings.h>
-#endif
