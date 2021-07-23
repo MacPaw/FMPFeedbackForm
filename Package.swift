a361a5b54874e98de326a8c885ae2c09c5e9aad9
Serhii Popov <sergi@macpaw.com>
2021-07-23 21:13:50 +0300

    Fix SPM build

--- Package.swift
+++ Package.swift
@@ -21,7 +21,11 @@ let package = Package(
             name: "FMPFeedbackForm",
             path: "FMPFeedbackForm",
             exclude: ["Info.plist"],
-            resources: [.process("Resources")]
+            resources: [.process("Resources")],
+            cSettings: [
+                .headerSearchPath("FMPFeedbackForm"),
+                .headerSearchPath("FMPFeedbackForm/include"),
+            ]
         )
     ]
 )
